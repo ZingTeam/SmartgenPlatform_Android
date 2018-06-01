5e00c7ebe7d34e23d6589a7314da65d39150b479
baozi <1300741099@qq.com>
2018-06-01 16:49:32 +0800

    完成注册页面和登录页面

---
+++ app/src/main/java/com/example/txjju/smartgenplatform_android/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.txjju.smartgenplatform_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.txjju.smartgenplatform_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
