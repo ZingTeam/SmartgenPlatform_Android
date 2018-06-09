37aba643463a750e6fec1cd9e58474262cdf89a6
Administrator <2339523997@qq.com>
2018-06-09 21:36:33 +0800

    确认订单和订单成功

---
+++ app/src/main/java/com/example/txjju/smartgenplatform_android/activity/ConfirmOrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.txjju.smartgenplatform_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.txjju.smartgenplatform_android.R;
+
+public class ConfirmOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_order);
+    }
+}
